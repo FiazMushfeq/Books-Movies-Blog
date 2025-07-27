97c23c0c7a4df4fe59f12eae785799c481ca64f9
FiazMushfeq <fiazmushfeq@live.com>
2025-07-27 15:34:43 -0400

    add concurr for backend and frontend

--- start.sh
+++ start.sh
@@ -6,7 +6,14 @@ brew services start mongodb-community
 # Start backend server
 cd backend
 npm start &
+BACKEND_PID=$!
 
 # Start React frontend
 cd ../frontend
 npm start &
+FRONTEND_PID=$!
+
+# Trap Ctrl+C and kill backend/frontend, plus any process on ports 3000/5050
+trap "kill $BACKEND_PID $FRONTEND_PID; kill \$(lsof -ti :3000); kill \$(lsof -ti :5050); exit" SIGINT
+
+wait
