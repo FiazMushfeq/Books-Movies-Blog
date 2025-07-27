2677f1d20c4b9b6c547d3666bf31a2faca4a6d46
FiazMushfeq <fiazmushfeq@live.com>
2025-07-27 01:34:39 -0400

    created frontend and backend starting code

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Example route import
+const exampleRoutes = require('./routes/example');
+app.use('/api/example', exampleRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+mongoose.connect('mongodb://localhost:27017/books-movies-blog', {});
+
+app.listen(5050, () => {
+  console.log('Server started on port 5050');
+});
